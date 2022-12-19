2101a0a2fff85954d5e2a0d0a3a67c45d98ed6a5
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-19 16:06:55 +0100

    Add files via upload

--- pages/0_Original_Manual.py
+++ pages/0_Original_Manual.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 15:54:27 2022
+
+@author: namnguyen
+"""
+import streamlit as st
+import base64
+
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+          base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf">'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+show_pdf("Input_files/carsecn.pdf")
+
 
