9e591474edbdf5b3a2ba3a1daf9322afbcc4d188
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-20 14:54:50 +0100

    Update 0_Original_Manual.py

--- pages/0_Original_Manual.py
+++ pages/0_Original_Manual.py
@@ -10,10 +10,11 @@ import base64
 
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
-          base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="1000" type="application/pdf">'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>'
+    
+    st.markdown(pdf_display, unsafe_allow_html=True)
     
     
 st.write("This is the original pdf")
