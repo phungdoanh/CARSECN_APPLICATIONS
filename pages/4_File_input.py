2cd5694b5b9761d07d94a09296efb2262011c8c3
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-20 15:43:45 +0100

    Add files via upload

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -20,20 +20,7 @@ from zipfile import ZipFile
 
 st.subheader('Download Excel Template input')
 
-
-# Input excel 
-#input_DB = pd.read_excel(
-#    r'Input_files/CARSEC_excel.xlsx',
- #   sheet_name=None, header=0, index_col=None)
-#Show all tables in excel
-#list_tables=list(input_DB.keys())
-
-#for k in list_tables:  
- #   st.write(k)
- #   st.write(input_DB[k].head(10))
-
-
-with open("Input_files/CARSEC_excel.xlsx", "rb") as fp:
+with open("Input_files\CARSEC_excel.xlsx", "rb") as fp:
 	btn = st.download_button(label="Download Excel Template",data=fp,file_name="CARSEC_Excel_Input.xlsx",mime="application/xlsx")
 	
 st.subheader('Upload Excel input')
@@ -49,23 +36,23 @@ if uploaded_file is not None:
 
 st.subheader('Download CARSEC files')
 
-path='Output_files/Multi_CARSEC'
+path='Output_files\Multi_CARSEC'
 if os.path.exists(path):
 	dirs = os.listdir(path)
 	for file in dirs:
-		os.remove(path+'//'+file)
+		os.remove(path+'\\'+file)
 		
 
 if uploaded_file is not None:
-	CS.excel_to_CARSEC(load_path=uploaded_file,export_path='Output_files/Multi_CARSEC/CS_Multi_')
+	CS.excel_to_CARSEC(load_path=uploaded_file,export_path='Output_files\Multi_CARSEC\CS_Multi_')
 
 
 dirs = os.listdir(path)
-with ZipFile('Output_files/ARSEC_multi.zip', 'w') as zipObj:
+with ZipFile('Output_files\CARSEC_multi.zip', 'w') as zipObj:
 	# Add multiple files to the zip
 	for file in dirs:
 		st.write(file)
-		zipObj.write('Output_files/Multi_CARSEC'+'//'+file)
+		zipObj.write('Output_files\Multi_CARSEC'+'\\'+file)
 	
-with open('Output_files/CARSEC_multi.zip', "rb") as fp:
+with open('Output_files\CARSEC_multi.zip', "rb") as fp:
 	btn = st.download_button(label='Download CARSEC files',data=fp,file_name="CARSEC_multi.zip",mime="application/ZIP")
