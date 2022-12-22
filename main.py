a2a9f08a75f55977b535c25602ab2d4bed318c39
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-22 11:32:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ Created on Tue Dec 13 10:16:07 2022
 
 
 import streamlit as st
+import base64
 
 st.write("***THIS IS THE MAIN PAGE***")
 
 
+
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>'
+    
+    st.markdown(pdf_display, unsafe_allow_html=True)
+    
+    
+st.write("The original pdf")
+    
+    
+show_pdf("Input_files/carsecn.pdf")
 
