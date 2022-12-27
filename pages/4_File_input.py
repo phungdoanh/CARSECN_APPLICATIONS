7428528ecdb61db1da9dd2f9c642d821a66f9ef7
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-27 17:50:22 +0100

    Update 4_File_input.py

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -45,8 +45,8 @@ temple_multi=tempfile.gettempdir() +'/CARSEC_multi.zip'
 with ZipFile(temple_multi, 'w') as zipObj:
  	# Add multiple files to the zip
  	for file in dirs:
-		filePath = os.path.join(temple_multi, file)
- 		zipObj.write(filePath)
+		
+ 		zipObj.write(temple_multi+'\\'+file)
 
 
 with open(temple_multi, "rb") as fp:
