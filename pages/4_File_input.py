f82d37913b9692e58ffbde4a955c4fc13fd8f79b
phungdoanh <phungdoanh@gmail.com>
2022-12-28 19:48:55 +0700

    Update 4_File_input.py

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -49,13 +49,13 @@ if uploaded_file is not None:
 
 path2=os.walk("Output_files/Multi_CARSEC")
 # dirs = os.listdir(path)
-with ZipFile('Output_files/CARSEC_multi.zip', 'w') as zipObj:
+with ZipFile('CARSEC_multi.zip', 'w') as zipObj:
 	# Add multiple files to the zip
 	for file in path2:
 		st.write(file)
-		zipObj.write('Output_files\Multi_CARSEC'+'//'+file)
+		zipObj.write(file)
 	
-with open('Output_files/CARSEC_multi.zip', "rb") as fp:
+with open('CARSEC_multi.zip', "rb") as fp:
 	btn = st.download_button(label='Download CARSEC files',data=fp,file_name="CARSEC_multi.zip",mime="application/ZIP")
 
 
