c8f248da7de9bc4d3829fdfe3041f09bba28e4cf
phungdoanh <phungdoanh@gmail.com>
2022-12-28 10:08:21 +0700

    Update 4_File_input.py

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -36,9 +36,9 @@ st.subheader('Download Muti CARSEC files')
 if uploaded_file is not None:
 	multi_name_file = tempfile.gettempdir()
 	#CS.CARSEC_Writer(DB=DB, export_path=name_file)
-	CS.excel_to_CARSEC(load_path=uploaded_file,export_path=multi_name_file+'/CS_Multi_.xls')
+	CS.excel_to_CARSEC(load_path=uploaded_file,export_path=multi_name_file+'/CS_Multi.xls')
 	with ZipFile('CARSEC_multi.zip', 'w') as zipObj:
-		zipObj.write(multi_name_file+'/CS_Multi_.xls')
+		zipObj.write(multi_name_file+'/CS_Multi.xls')
 		
 	
 
