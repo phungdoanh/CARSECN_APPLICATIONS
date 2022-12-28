9131c8341c46c7e7bc8f56b8e753ad4037aa6d02
phungdoanh <phungdoanh@gmail.com>
2022-12-28 19:51:52 +0700

    Update 4_File_input.py

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -33,10 +33,10 @@ if uploaded_file is not None:
 #%%%%%%%%%%%%%%%%%%%
 st.subheader('Download Muti CARSEC files')
 
-if uploaded_file is not None:
-	multi_name_file = tempfile.gettempdir()
-	#CS.CARSEC_Writer(DB=DB, export_path=name_file)
-	CS.excel_to_CARSEC(load_path=uploaded_file,export_path=multi_name_file+'/CS_Multi_')	
+# if uploaded_file is not None:
+# 	multi_name_file = tempfile.gettempdir()
+# 	#CS.CARSEC_Writer(DB=DB, export_path=name_file)
+# 	CS.excel_to_CARSEC(load_path=uploaded_file,export_path=multi_name_file+'/CS_Multi_')	
 
 #dirs=tempfile.gettempdir()
 
