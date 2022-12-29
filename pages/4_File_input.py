a7a0b2ffcd0977e4609864a2197e5290deb61dc3
phungdoanh <phungdoanh@gmail.com>
2022-12-29 08:00:55 +0700

    Update 4_File_input.py

--- pages/4_File_input.py
+++ pages/4_File_input.py
@@ -95,7 +95,7 @@ path='/app/carsecn_applications/Output_files/Multi_CARSEC'
 if os.path.exists(path):
 	dirs = os.listdir(path)
 	for file in dirs:
-		os.remove(path+'\\'+file)
+		os.remove(path+'/'+file)
 		
 
 if uploaded_file is not None:
@@ -107,7 +107,7 @@ with ZipFile('Output_files\CARSEC_multi.zip', 'w') as zipObj:
 	# Add multiple files to the zip
 	for file in dirs:
 		st.write(file)
-		zipObj.write('Output_files\Multi_CARSEC'+'\\'+file)
+		zipObj.write('Output_files\Multi_CARSEC'+'/'+file)
 	
 with open('Output_files\CARSEC_multi.zip', "rb") as fp:
 	btn = st.download_button(label='Download CARSEC files',data=fp,file_name="CARSEC_multi.zip",mime="application/ZIP")
